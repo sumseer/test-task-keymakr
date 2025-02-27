8c5bc6ca99e976be0ce8ba0d046d3b11d94fda03
Max Savenya <savenya2013@gmail.com>
2025-02-27 19:55:02 +0200

    feat: Create a separate main file working as an app entry point

---
+++ src/fetch_api/main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from src.fetch_api.data_fetching import fetch_data, validate_posts
+from src.fetch_api.data_saving import save_posts_to_csv, save_posts_to_db
+
+
+API_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+async def main():
+    """Main function to fetch, validate, and save data."""
+    posts = await fetch_data(API_URL)
+
+    valid_posts = validate_posts(posts)
+
+    save_posts_to_csv(valid_posts)
+    print("Data saved to posts.csv")
+
+    save_posts_to_db(valid_posts)
+    print("Data saved to SQLite database")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
