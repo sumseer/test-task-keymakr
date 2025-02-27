88599be401c4be807d5878c8b9a1ca864e2e8a1b
Max Savenya <savenya2013@gmail.com>
2025-02-27 15:30:39 +0200

    fix: Update API request response error

--- src/fetch_api/exceptions.py
+++ src/fetch_api/exceptions.py
@@ -32,7 +32,7 @@ class APIRequestResponseError(BaseAPIError):
     """Raised when an API response is invalid or unexpected."""
 
     def __init__(self, status_code: int, message=None):
-        if message is None:
-            message = f"Unexpected API response: {status_code}"
-        super().__init__(message)
         self.status_code = status_code
+        if message is None:
+            message = f"Unexpected API response with status code {status_code}."
+        super().__init__(message)
